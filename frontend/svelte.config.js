37473ffd85e8c569b83b77a350275abd7388c05c
elemoser <elemoser@gmail.com>
2023-11-21 14:03:19 +0100

    Switch to static adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// The default "adapter-auto" only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		// For this project the static adapter was chosen, which will prerender the entire site as a collection of static files.
+		// To prerender only some pages and dynamically server-render others, we can use prerender option
+		// https://kit.svelte.dev/docs/page-options#prerender.
+
+		adapter: adapter({
+			// Default options
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
