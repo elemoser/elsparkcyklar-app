d5f16afe12feb6dbd15f617057f044a54a976403
Yli <jeyl22@bth.se>
2023-11-30 18:14:54 +0100

    profile route and page

---
+++ frontend/src/routes/(app)/profile/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    //TODO Get signed in user with their user id.
+    const user = {
+        ssn: 123123123,
+        name: "Pelle",
+        surname: "Skrutt",
+        phone: "07012312340",
+        mail: "Pelle.skrutt@vmail.com",
+        balance: 1337,
+        subscriber: true
+    };
+</script>
+
+<div class="profile-container">
+    <div class="user-div">
+        <!-- TODO Add user name, phone, email so that it is visible on the screen. -->
+        <!-- TODO Add a button that leads to a form that allows the user to update their info. -->
+    </div>
+    <div class="menu-div">
+        <!-- TODO Add a menu with options like: subscribe, add payment, history, invoice -->
+    </div>
+</div>
