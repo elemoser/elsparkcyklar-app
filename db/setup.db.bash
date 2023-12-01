9996e827297034a57d858a90f8937af35a5ddc19
elemoser <elemoser@gmail.com>
2023-12-01 18:17:57 +0100

    Add city coordinates to db

--- db/setup.db.bash
+++ db/setup.db.bash
@@ -1,20 +1,52 @@
 #!/usr/bin/env bash
 
-DBFILE="bikr.db"
+DBFILE="new_bikr.db"
 THISFILENAME=$(basename "$0") # Get file name
 
-# Remove any existing db named bikr.db
-if [[ -f $DBFILE ]]; then
-    rm -f $DBFILE
-fi
+# Remove any existing db of the same name
+rm -f $DBFILE
+
+# GEODATA
+CITIES=(Stockholm Göteborg Malmö Uppsala Linköping)
+IDS=(R398021 R935611 R10663667 R305455 R935467)
 
 # Create and populate a new database named bikr.db
 touch $DBFILE
 sqlite3 $DBFILE < sql/create-tables.sql
+
+# Create city table with nominatim data
+index=1
+
+for i in "${!IDS[@]}"
+do 
+    name=${CITIES[$i]}
+    id=${IDS[$i]}
+    data=$(curl -s "https://nominatim.openstreetmap.org/lookup?osm_ids=$id&polygon_geojson=1&format=json")
+    if [ "$(echo "$data" | jq -e '. == []')" = "true" ]; then
+        echo "Error: Something went wrong with the nominatim API."
+        rm -f $DBFILE
+        exit 1
+    else
+        echo "$data" > "data/$name.json"
+        bounds=$(jq -cr '.[0].geojson.coordinates[0]' "data/$name.json")
+        sqlite3 $DBFILE "INSERT INTO city(id, name, bounds) VALUES($index,'$name','$bounds');"
+    fi
+    ((index=index+1))
+done
+
+# Import rest of data dependent on CSV data above
 sqlite3 $DBFILE < sql/mock-data.sql
+
+sqlite3 $DBFILE "SELECT * FROM city;"
+
+# Remove old bikr.db
+rm -f bikr.db
+# Rename newly created db to bikr.db
+mv $DBFILE bikr.db
 
 # Check the exit status
 if [ $? -eq 0 ]; then
+    
     echo "$THISFILENAME executed successfully."
 else
     echo "$THISFILENAME encountered an error."
