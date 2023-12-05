67929c94709fc56e9379d13b9cca6785613c0a7a
elemoser <elemoser@gmail.com>
2023-12-05 16:08:19 +0100

    Create form component for CRUD

--- frontend/src/routes/admin/[info]/+page.svelte
+++ frontend/src/routes/admin/[info]/+page.svelte
@@ -1,16 +1,18 @@
 <script>
+    import Form from '$lib/components/Form.svelte';
     /** @type {import('./$types').PageData} */
     export let data;
     let type = '';
 
     if (data.props.target.charAt(0) == 'k') {
-        type = 'user'
+        type = 'user';
     }
 </script>
 
 {#if data}
     <h1>Details for { type } { data.props.target.slice(1) }</h1>
-    {#each Object.entries(data.props.data) as [key, value]}
+    <!-- {#each Object.entries(data.props.data) as [key, value]}
         <p>{ key }: { value }</p>
-    {/each}
+    {/each} -->
+    <Form data={ data.props.data } />
 {/if}
