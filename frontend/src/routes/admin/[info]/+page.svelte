43ab010cdee8affc2ec6a974e15af69c05363fdf
elemoser <elemoser@gmail.com>
2023-12-05 17:12:49 +0100

    Remove unnecessary code

--- frontend/src/routes/admin/[info]/+page.svelte
+++ frontend/src/routes/admin/[info]/+page.svelte
@@ -11,8 +11,5 @@
 
 {#if data}
     <h1>Details for { type } { data.props.target.slice(1) }</h1>
-    <!-- {#each Object.entries(data.props.data) as [key, value]}
-        <p>{ key }: { value }</p>
-    {/each} -->
     <Form data={ data.props.data } />
 {/if}
