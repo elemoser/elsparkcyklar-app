efeb26cc0aa34e4574d6cc4e11d466479aa089d9
emklemkl <emil94_karlsson@outlook.com>
2023-12-16 15:36:48 +0100

    update the setup.bash to run json.to.db.bash and by doing that putting all the sim-routes in the db as separate objects. updated create-tables to include a tables for the simulate data. Also saved all sim-routes data in bike-sim-routes.json to avoid having 3000 different json files.

--- db/json.to.db.bash
+++ db/json.to.db.bash
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
-
-DBFILE="new_bikr.db"
-JSONFILES="data/bike-routes/*"
+TRIPS_JSONFILE="./data/bike-routes/bike-sim-routes.json"
+DATABASE="./bikr.db"
+DB_TABLE="simulate"
 THISFILENAME=$(basename "$0") # Get file name
-for JSON in $JSONFILES; do
-    trip=$(jq '{ city: .city, trips: .trips[].coords }' "$JSON")
-    echo "$trip"
+
+# Read the content of the JSON file using jq
+json=$(jq '.[]' "$TRIPS_JSONFILE")
+
+# Loop through each object in the JSON array
+for trip in $(echo "$json" | jq -c '.'); do
+
+        # Insert data into simulate table
+        city_id=$(echo "$trip" | jq -r '.city')
+        trip=$(echo "$trip" | jq -r '.trips')
+    sqlite3 $DATABASE <<EOF
+    INSERT INTO $DB_TABLE (city_id, bike_route) VALUES (
+        "$city_id",
+        "$trip"
+    );
+EOF
 done
+
 # Check the exit status
 if [ $? -eq 0 ]; then
     
