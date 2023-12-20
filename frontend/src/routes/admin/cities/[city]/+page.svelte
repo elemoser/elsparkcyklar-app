63c985fe54b99949ea447d4ce0409839e2d6f0e8
elemoser <elemoser@gmail.com>
2023-12-20 10:51:21 +0100

    Remove edit or delete option for cities

--- frontend/src/routes/admin/cities/[city]/+page.svelte
+++ frontend/src/routes/admin/cities/[city]/+page.svelte
@@ -5,24 +5,6 @@
 	/** @type {import('./$types').PageData} */
 	export let data;
 	let mapData = {};
-	let check = false;
-	let edit = false;
-
-	// Delete given row from db
-	async function removeCity(id) {
-		// TODO fix this once this is resolved in the backend
-		// // Note that localhost instead of server needs to be used here
-		// const response = await fetch(`http://localhost:1338/v1/city/id/${id}`, {
-		//     method: "DELETE"
-		// });
-		// if (response.status === 200) {
-		//     // redirect
-		//     goto('/admin/cities');
-		// }  else {
-		//     console.log(`Failed to delete city ${id}:`, response.statusText);
-		//     //TODO error handling
-		// }
-	}
 
 	if (data.props.data.bounds) {
 		mapData['polygon'] = {
@@ -43,29 +25,10 @@
 		</label>
 		<label for="name"
 			>Namn
-			<input id="name" name="name" type="text" value={data.props.data.name} readonly={!edit} />
+			<input id="name" name="name" type="text" value={data.props.data.name} readonly/>
 		</label>
-		{#if edit}
-			<label for="osm"
-				>Stadens gräns (osmtype + osmid från <a
-					href="https://nominatim.openstreetmap.org/ui/search.html">nominatim</a
-				>)
-				<input id="osm" name="osm" type="text" value="" readonly={!edit} />
-			</label>
-			<input type="submit" value="Spara" />
-		{/if}
 	</form>
-	{#if check}
-		<p>Är du säker på att du vill ta bort denna stad från databasen?</p>
-		<button on:click={removeCity(data.props.data.id)}>Radera</button>
-		<button on:click={() => (check = false)}>Avbryt</button>
-	{:else if edit}
-		<button on:click={() => (edit = false)}>Avbryt</button>
-	{:else}
-		<button on:click={() => (check = true)}>Ta bort</button>
-		<button on:click={() => (edit = true)}>Redigera</button>
-		<button><a href="/admin/cities">Avbryt</a></button>
-	{/if}
+	<button><a href="/admin/cities">Gå tillbaka</a></button>
 	<LeafletMap data={mapData} />
 {/if}
 
