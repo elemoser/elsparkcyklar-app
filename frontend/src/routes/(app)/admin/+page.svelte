0f4314437b6afddcfc8d7476b536f3cecf0043ee
elemoser <elemoser@gmail.com>
2023-12-28 17:02:45 +0100

    Fix wordbreak on small screens

--- frontend/src/routes/(app)/admin/+page.svelte
+++ frontend/src/routes/(app)/admin/+page.svelte
@@ -2,7 +2,7 @@
     import {app_name} from '$lib/index.js';
 </script>
 
-<h1>Välkommen till administratörssidan för {app_name} appen</h1>
+<h1>Välkommen till adminsidan för {app_name} appen</h1>
 
 <p>
     Härifrån kan du hantera kundkonton, alla bokningar och reseavgifter samt alla cyklar, aktiva städer och parkeringsstationer.
@@ -13,6 +13,7 @@
     h1,
     p {
         color: white;
+		hyphens: auto;
     }
 
     a,
