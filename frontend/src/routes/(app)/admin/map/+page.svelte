502c7840e629d4dc318a4c660085d1326e59f002
elemoser <elemoser@gmail.com>
2023-12-28 14:04:24 +0100

    Update map view

--- frontend/src/routes/(app)/admin/map/+page.svelte
+++ frontend/src/routes/(app)/admin/map/+page.svelte
@@ -8,26 +8,31 @@
 	let map;
 	let bikeLayer;
 	let parkingLayer;
-	// let scooterIcon;
 	let showBikes = false;
 	let showParkings = false;
+
     onMount(async () => {
         // Create a map
         if (browser) {
 			L = await import('leaflet');
+
 			// Coordinates for the maps view
             // For the current example (zoom on Östermalm)
 			let lat = 59.33808;
 			let lon = 18.08996;
+
 			map = L.map(mapElement);
+
 			// Create the map
 			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 				attribution:
 					'© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 			}).addTo(map);
+
 			// Create a layer for markers
 			bikeLayer = L.layerGroup().addTo(map);
 			parkingLayer = L.layerGroup().addTo(map);
+
 			if (data.props.data.city) {
 				let cities = data.props.data.city;
 				let bounds;
@@ -47,34 +52,31 @@
 						sortedLatLon.push([pair[1], pair[0]]);
 					}
 					// Create polygon
-					L.polygon(sortedLatLon, { color: '#9747ff', weight: 1, fill: false }).addTo(map);
+					L.polygon(sortedLatLon, { color: 'red', weight: 1, fill: false }).addTo(map);
 					}
 			}
 			map.setView([lat, lon], 5);
 		}
     });
+
     onDestroy(async () => {
 		if (map) {
 			// console.log('Unloading Leaflet map.');
 			map.remove();
 		}
 	});	
+
 	function toggleBikes() {
 		showBikes = !showBikes;
+
 		if (showBikes) {
-			// scooterIcon = L.icon({
-			// 	iconUrl: "/kick-scooter-icon.png",
-			// 	iconSize:     [20, 25], // size of the icon
-			// 	// iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-			// 	// popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-			// });
 			if (data.props.data.bike) {
 				let coordinates = [];
 				let text = '';
 				for (let key in data.props.data.bike) {
 					coordinates = data.props.data.bike[key].position.split(', ');
 					coordinates = coordinates.map((x) => parseFloat(x));
-					text = `Cykel ${data.props.data.bike[key].id}`;
+					text = `Cykel ${data.props.data.bike[key].id} (${data.props.data.bike[key].battery}%)`;
 					L.marker(coordinates).addTo(bikeLayer).bindPopup(text);
 				}
 			}
@@ -82,41 +84,37 @@
 			bikeLayer.clearLayers();
 		}
 	}
+
 	function toggleParking() {
 		showParkings = !showParkings;
 		
 		if (showParkings) {
 			let	parkingIcon = L.icon({
 				iconUrl: "/parking-area.png",
-				iconSize:     [30, 35], // size of the icon
-				// iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-				// popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+				iconSize: [30, 35]
 			});
 			if (data.props.data.parking) {
 				let coordinates = [];
 				let text = '';
-				let rectangle;
+
 				for (let key in data.props.data.parking) {
-					coordinates = data.props.data.parking[key].bounds.split(', ');
-					coordinates = coordinates.map((x) => parseFloat(x));
+					coordinates = data.props.data.parking[key].center.split(', ');
 					text = `${data.props.data.parking[key].name} (${data.props.data.parking[key].number_of_chargers} laddare)`;
-					// Create rectangle
-					rectangle = L.rectangle([[coordinates[0],coordinates[1]],[coordinates[2],coordinates[3]]], {color: 'yellow', weight: 1, fill: false}).addTo(parkingLayer);
-					// Create marker in center of rectangle
-					coordinates = rectangle.getCenter();
-					L.marker([coordinates.lat, coordinates.lng], {icon: parkingIcon}).addTo(parkingLayer).bindPopup(text);
+					L.marker([coordinates[0], coordinates[1]], {icon: parkingIcon}).addTo(parkingLayer).bindPopup(text);
+					L.circle([coordinates[0], coordinates[1]], data.props.data.parking[key].radius, { stroke: false, color: 'orange' }).addTo(parkingLayer);
 				}
 			}
 		} else {
 			parkingLayer.clearLayers();
 		}
 	}
+
 	function zoomIn(e) {
 		e.preventDefault();
 		const formData = new FormData(e.target);
-		const lat = formData.get('lat');
-		// const lon = formData.get('lon');
-		let coordinates = lat.split(', ');
+		const latLon = formData.get('lat_lon');
+		let coordinates = latLon.split(', ');
+
 		coordinates = coordinates.map((x) => parseFloat(x));
 		map.flyTo(coordinates, 14);
 	}
@@ -125,8 +123,7 @@
 <form class="submit-form-online" on:submit={zoomIn}>
 	<label>
 		Zoom
-		<input name="lat" type="text" placeholder="lat, lon" required>
-		<!-- <input name="lon" type="number" step="0.000001" placeholder="lon" required> -->
+		<input name="lat_lon" type="text" placeholder="lat, lon" required>
 	</label>
 	<input type="submit" value="Ta mig dit">
 </form>
@@ -144,8 +141,7 @@
 
 <div class="map" bind:this={mapElement} />
 <div>
-	<a href="https://www.flaticon.com/free-icons/map-and-location" title="map and location icons">Map and location icons created by ono_tono - Flaticon</a>
-	<a href="https://www.flaticon.com/free-icons/parking" title="parking icons">Parking icons created by ono_tono - Flaticon</a>
+	<a href="https://www.flaticon.com/free-icon/parking-area_4979378?related_id=4979378" target=”_blank” title="parking icons">Parking icons created by ono_tono - Flaticon</a>
 </div>
 
 <style>
