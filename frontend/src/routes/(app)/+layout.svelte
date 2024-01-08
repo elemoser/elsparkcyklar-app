471da83a936451e93685cd373d9a641ba6176c81
Yli <jeyl22@bth.se>
2024-01-08 22:55:50 +0100

    Internal Error fix when rebooting server

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -3,8 +3,13 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import { page } from '$app/stores';
 	import { user } from '$lib/stores/user';
+	import { goto } from '$app/navigation';
 	$: userData = $user;
 	$: check = $page.url.pathname.startsWith('/admin');
+
+	if ($user.error === true) {
+		goto('/login');
+	}
 </script>
 
 {#if check}
